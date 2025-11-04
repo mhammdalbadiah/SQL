1a152c881f3ca37fbc45ca9529befd47c79d0296
mhammd AL-badiah <mhammdqqpp@gmail.com>
2025-11-04 22:36:03 +0300

    Add files via upload

---
+++ first_Draft.sql
@@ -0,0 +1,17 @@
+  CREATE DATABASE MU;    
+USE MU ;
+
+ CREATE TABLE prof(
+prof_ID int PRIMARY KEY ,
+prof_F_NAME VARCHAR (50),
+prof_L_NAME VARCHAR (50),
+prof_EMAIL  VARCHAR (50), 
+prof_MAJOR  VARCHAR (50)
+
+);
+ 
+ 
+ SELECT *FROM prof ; 
+
+
+ 
